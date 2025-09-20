39735d50ae8f83ff360dc46857573b3cce9775ac
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-09-20 11:59:04 -0700

    Add a console app

---
+++ AssetRipper.Conversions.UnityCrunch.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AssetRipper.Conversions.UnityCrunch.ConsoleApp;
+
+internal static class Program
+{
+	private static void Main(string[] args)
+	{
+		byte[] data = File.ReadAllBytes(args[0]);
+		string outputPath = args.Length > 1 ? args[1] : args[0] + ".dxt";
+		if (UnityCrunch.TryDecompress(data, out byte[]? output))
+		{
+			File.WriteAllBytes(outputPath, output);
+			Console.WriteLine($"Decompressed to {outputPath}");
+		}
+		else
+		{
+			Console.WriteLine("Decompression failed");
+		}
+	}
+}
